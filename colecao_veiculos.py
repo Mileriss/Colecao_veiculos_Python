9ffe9aceab263a00c5820e4d9fd995d5535a1777
Rafael Mileris <rafaelmileris15@gmail.com>
2022-12-18 13:12:08 -0300

    Adicionando a funcao 'infoVeiculo'

--- colecao_veiculos.py
+++ colecao_veiculos.py
@@ -17,6 +17,7 @@ def addVeiculo():
     print("Adicionar novo veiculo\n\n")
     selc_veic = input("Selecione o tipo de veiculo: \n1 - Carro\n2 - Moto\n3 - Bicicleta\nDigite aqui: ")
     if selc_veic == "1":
+        os.system('cls')
         print("Digite o modelo do carro\n")
         mod_carro = input("Digite aqui: ")
         print("Digite a marca do carro\n")
@@ -29,6 +30,7 @@ def addVeiculo():
         lista_carro.append(carro)
         print("\n\nVeiculo adicionado!")
     elif selc_veic == "2":
+        os.system('cls')
         print("Digite o modelo da moto\n")
         mod_moto = input("Digite aqui: ")
         print("Digite a marca da moto\n")
@@ -41,6 +43,7 @@ def addVeiculo():
         lista_moto.append(moto)
         print("\n\nVeiculo adicionado!")
     elif selc_veic == "3":
+        os.system('cls')
         print("Digite o modelo da bicicleta\n")
         mod_bici = input("Digite aqui: ")
         print("Digite a cor da bicicleta\n")
@@ -50,3 +53,19 @@ def addVeiculo():
         bicicleta = Bicicleta(mod_bici, cor_bici, str(rodas_bici))
         lista_bicicleta.append(bicicleta)
         print("\n\nVeiculo adicionado!")
+    else:
+        print("Algo deu errado...")
+
+def infoVeiculo():
+    selc_veic = input("Selecione o tipo de veiculo: \n1 - Carro\n2 - Moto\n3 - Bicicleta\nDigite aqui: ")
+    if selc_veic == "1":
+        for carro in lista_carro:
+            print(f"- {carro}")
+    elif selc_veic == "2":
+        for moto in lista_moto:
+            print(f"- {moto}")
+    elif selc_veic == "3":
+        for bike in lista_bicicleta:
+            print(f"- {bike}")
+    else:
+        print("Algo deu errado...")
