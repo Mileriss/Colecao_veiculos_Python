289d19d45263851ca461a3cf215b32b2ae64184b
Rafael Mileris <rafaelmileris15@gmail.com>
2022-12-18 12:19:33 -0300

    alterando a estrutura

--- colecao_veiculos.py
+++ colecao_veiculos.py
@@ -1,37 +1,52 @@
 #Importando a biblioteca OS
 import os
+from Carro import *
+from Moto import *
+from Bicicleta import *
 
-#Criando a classe de veículo: Carro
-class Carro:
-    modelo_carro = ""
-    marca_carro = ""
-    cor_carro = ""
-    vel_max_carro = 0
-    def __init__(self, modelo_carro, marca_carro, cor_carro, vel_max_carro):
-        self.modelo = modelo_carro
-        self.marca = marca_carro
-        self.cor = cor_carro
-        self.vel_max = vel_max_carro
+def menu():
+    print("MENU - Selecione uma opcao\n\n")
+    print("1 - Adicionar novo veiculo")
+    print("2 - Info. Veiculo")
+    print("3 - Deletar veiculo")
+    print("4 - Listar veiculos")
+    print("5 - Finalizar programa")
 
-#Criando a classe de veículo: Moto
-class Moto:
-    modelo_moto = ""
-    marca_moto = ""
-    cor_moto = ""
-    vel_max_moto = 0
-    def __init__(self, modelo_moto, marca_moto, cor_moto, vel_max_moto):
-        self.modelo = modelo_moto
-        self.marca = marca_moto
-        self.cor = cor_moto
-        self.vel_max = vel_max_moto
-
-#Criando a classe de veículo: Bicicleta
-class Bicicleta:
-    modelo_bike = ""
-    cor_bike = ""
-    qtde_rodas_bike = 0
-    def __init__(self, modelo_bike, cor_bike, qtde_rodas_bike):
-        self.modelo = modelo_bike
-        self.cor = cor_bike
-        self.qtde_rodas = qtde_rodas_bike
-
+def addVeiculo():
+    os.system('cls')
+    print("Adicionar novo veiculo\n\n")
+    selc_veic = input("Selecione o tipo de veiculo: \n1 - Carro\n2 - Moto\n3 - Bicicleta\nDigite aqui: ")
+    if selc_veic == "1":
+        print("Digite o modelo do carro\n")
+        mod_carro = input("Digite aqui: ")
+        print("Digite a marca do carro\n")
+        marca_carro = input("Digite aqui: ")
+        print("Digite a cor do carro\n")
+        cor_carro = input("Digite aqui: ")
+        print("Digite a velocidade do carro\n")
+        vel_carro = int(input("Digite aqui: "))
+        carro = Carro(mod_carro, marca_carro, cor_carro, str(vel_carro))
+        lista_carro.append(carro)
+        print("\n\nVeiculo adicionado!")
+    elif selc_veic == "2":
+        print("Digite o modelo da moto\n")
+        mod_moto = input("Digite aqui: ")
+        print("Digite a marca da moto\n")
+        marca_moto = input("Digite aqui: ")
+        print("Digite a cor da moto\n")
+        cor_moto = input("Digite aqui: ")
+        print("Digite a cor da moto\n")
+        vel_moto = int(input("Digite aqui: "))
+        moto = Moto(mod_moto, marca_moto, cor_moto, str(vel_moto))
+        lista_moto.append(moto)
+        print("\n\nVeiculo adicionado!")
+    elif selc_veic == "3":
+        print("Digite o modelo da bicicleta\n")
+        mod_bici = input("Digite aqui: ")
+        print("Digite a cor da bicicleta\n")
+        cor_bici = input("Digite aqui: ")
+        print("Digite a quantidade de rodas da bicicleta\n")
+        rodas_bici = int(input("Digite aqui: "))
+        bicicleta = Bicicleta(mod_bici, cor_bici, str(rodas_bici))
+        lista_bicicleta.append(bicicleta)
+        print("\n\nVeiculo adicionado!")
