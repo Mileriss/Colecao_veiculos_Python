0764f75eceab1234011d8a6dda69e6e0fd98a553
Rafael Mileris <rafaelmileris15@gmail.com>
2022-12-19 17:22:25 -0300

    Diversas alterações realizadas

--- Carro.py
+++ Carro.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Lista para armazenar os carros
 lista_carro = []
 
@@ -11,22 +10,6 @@ class Carro:
     def __init__(self, marca_carro, modelo_carro, cor_carro, vel_max_carro):
         self.marca = marca_carro
         self.modelo = modelo_carro
-=======
-import os
-
-#Lista para armazenar os carros
-lista_carro = [["Automatico", "Mercedez", "Preto", 200]]
-
-#Criando a classe de veículo: Carro
-class Carro:
-    modelo_carro = ""
-    marca_carro = ""
-    cor_carro = ""
-    vel_max_carro = 0
-    def __init__(self, modelo_carro, marca_carro, cor_carro, vel_max_carro):
-        self.modelo = modelo_carro
-        self.marca = marca_carro
->>>>>>> ba743fe4602617482865126b971cc3fdb3cf9306
         self.cor = cor_carro
         self.vel_max = vel_max_carro
 
